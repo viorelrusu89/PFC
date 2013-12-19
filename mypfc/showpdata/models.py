88b7c9aeb9481e473ab30ce83cc11b347a392bb1
Viorel <viorelrusu89@gmail.com>
2013-12-19 19:56:56 +0100

    first models version

--- mypfc/showpdata/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-#TODO: adaptar estas tablas a las de VizGrimoire https://github.com/MetricsGrimoire/CVSAnalY/wiki/Database-Schema
--- mypfc/showpdata/views.py~
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-from django.http import HttpResponse
-
-def users(request):
-
-def projects(request, user):
-
-def commits(request, user):
