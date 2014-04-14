8faa3bd5c77d75d80a3a89d7813d0bf0b4e92aeb
Viorel <viorelrusu89@gmail.com>
2014-04-14 23:30:03 +0200

    bootstrapped

--- mypfc/finalapp/static/js/controllers.js
+++ mypfc/finalapp/static/js/controllers.js
@@ -14,34 +14,62 @@ djangoAPIservice.getNCommits().success(function (response) {
 controller('timeseriesController', function($scope, djangoAPIservice) {
 	$scope.timeseries = 777;
 
+
+djangoAPIservice.getTimeSeries().success(function (response){
+		$scope.timeseries = response.values;
+
+});
+
       g = new Dygraph(
               document.getElementById("demodiv"),
               function() {
+
+                /*Trying to fill out data from server
+                var r = "date, commits\n";
+                for (var i=0; i<=response.values.length; i++) {
+                r += response.values[i][0] + "," + response.values[i][1][0] + "\n" ;
+                }
+                return r;
+                */
+                
+                //hand-written "object". TODO: generate it from server
+
+                r =  "date,ncommits\n" + 
+                     "2012-08-01, 16\n" +
+                     "2012-09-01, 21\n" +
+                     "2012-10-01, 9\n" +
+                     "2012-11-01, 2\n" +
+                     "2012-12-01, 33\n" +
+                     "2013-01-01, 6\n" +
+                     "2013-02-01, 6\n" +
+                     "2013-03-01, 75\n" +
+                     "2013-04-01, 196\n";
+                return r;
+
+                /*
+                // Dygraphs working model code
                 var zp = function(x) { if (x < 10) return "0"+x; else return x; };
-                var r = "date,parabola,line,another line,sine wave\n";
+                var r = "date,parabola\n";
                 for (var i=1; i<=31; i++) {
                 r += "200610" + zp(i);
                 r += "," + 10*(i*(31-i));
                 r += "\n";
+
                 }
                 return r;
+                */
+
               },
               {
                 strokeWidth: 2,
-                'parabola': {
+                'ncommits': {
                   strokeWidth: 0.0,
                   drawPoints: true,
-                  pointSize: 4,
+                  pointSize: 6,
                   highlightCircleSize: 6
                 },
               }
           );
 
 
-/*
-djangoAPIservice.getTimeSeries().success(function (response){
-		$scope.timeseries = response.values;
-
-});
-*/
 });
